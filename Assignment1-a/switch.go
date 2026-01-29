8743520b17cf81da8ab3bf3182095166de646b1d
surpurkarsanjananarendra <sanjana.surpurkar@coditas.com>
2026-01-30 00:14:09 +0530

    Added new subfolder with Go files

---
+++ Assignment1-a/switch.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func even(l1 []int) int {
+	sum := 0
+	for i, val := range l1 {
+		if i%2 == 0 {
+			sum += val
+		}
+	}
+	return int(sum)
+}
+
+func odd(l2 []int) int {
+	sum := 0
+	for i, val := range l2 {
+		if i%2 != 0 {
+			sum += val
+		}
+	}
+	return int(sum)
+}
+
+func main() {
+	l := [6]int{2, 9, 3, 4, 10, 1}
+
+	a := even(l[:])
+	b := odd(l[:])
+
+	switch {
+	case a > b:
+		fmt.Println("Sum of evens is greater!")
+	case b > a:
+		fmt.Println("Sum of odds is greater!")
+	default:
+		fmt.Println("Evens and odds sum up equal!")
+	}
+}
---
+++ Assignment1-b/select.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func even(l1 []int, c chan int) {
+	sum := 0
+	for i, val := range l1 {
+		if i%2 == 0 {
+			sum += val
+		}
+	}
+	time.Sleep(1 * time.Millisecond)
+	c <- sum
+}
+
+func odd(l2 []int, c chan int) {
+	sum := 0
+	for i, val := range l2 {
+		if i%2 != 0 {
+			sum += val
+		}
+	}
+	time.Sleep(2 * time.Millisecond)
+	c <- sum
+}
+
+func main() {
+	l := [6]int{2, 9, 3, 4, 10, 1}
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	go even(l[:], c1)
+	go odd(l[:], c2)
+
+	select {
+	case a := <-c1:
+		fmt.Println("Even may sum greater!", a)
+	case b := <-c2:
+		fmt.Println("Odd may sum greater!", b)
+	default:
+		fmt.Println("Evens and odds sum up equal!")
+	}
+}
